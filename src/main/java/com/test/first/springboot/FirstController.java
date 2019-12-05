e05b0ecb43b97fc3ae1145f84408bc19bbf6a8b3
kisuram <kiranmaisuram2@gmail.com>
2019-12-04 22:47:56 -0800

    cwm-000[kiranmai]some test changes

--- src/main/java/com/test/first/springboot/FirstController.java
+++ src/main/java/com/test/first/springboot/FirstController.java
@@ -56,7 +56,7 @@ public class FirstController {
         Map resultMap = new HashMap<>();
 
 
-        resultMap.put(22222222,"chinnu");
+        resultMap.put(12121212,"chinnu");
         resultMap.put(388,"rishi");
         resultMap.put(6678,"kannayya");
 
