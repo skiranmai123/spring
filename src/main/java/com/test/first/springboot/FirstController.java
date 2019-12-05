4e30afcf457de965555bbb95256e13e88099d9c3
kisuram <kiranmaisuram2@gmail.com>
2019-12-05 11:04:12 -0800

    cwm-000[kiranmai]some test changes

--- src/main/java/com/test/first/springboot/FirstController.java
+++ src/main/java/com/test/first/springboot/FirstController.java
@@ -24,7 +24,7 @@ public class FirstController {
 
         List apartment2 = new ArrayList();
         apartment2.add("kiranmai");
-        apartment2.add("erigadu");
+        apartment2.add("chinniiiiii");
         apartment2.add("suhaas");
 
         List apartment3 = new ArrayList();
