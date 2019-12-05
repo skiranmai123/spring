8f3ef7690ddaeb2325cac5bcd339137087f43a75
kisuram <kiranmaisuram2@gmail.com>
2019-12-04 21:47:46 -0800

    cwm-000[kiranmai]some test changes

--- src/main/java/com/test/first/springboot/FirstController.java
+++ src/main/java/com/test/first/springboot/FirstController.java
@@ -30,7 +30,7 @@ public class FirstController {
         List apartment3 = new ArrayList();
 
         resultMap.put("3355",apartment1);
-        resultMap.put("4455",apartment2);
+        resultMap.put("5666666888",apartment2);
         resultMap.put("6678",apartment3);
 
         return new ResponseEntity<>(resultMap.get(key), HttpStatus.OK);
